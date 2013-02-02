bc28535bb29573eaa081e439ca0d62a9ee47b8e3
Matt Kneiser <mattman@umich.edu>
2013-02-02 12:50:55 -0500

    Added FB integration

---
+++ public/js/fb.js
@@ -0,0 +1,37 @@
+// Load the SDK's source Asynchronously
+// Note that the debug version is being actively developed and might 
+// contain some type checks that are overly strict. 
+// Please report such bugs using the bugs tool.
+(function(d, debug){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
+   ref.parentNode.insertBefore(js, ref);
+ }(document, /*debug*/ false));
+
+window.fbAsyncInit = function() {
+  // init the FB JS SDK
+  FB.init({
+    appId      : '508101062566302', // App ID from the App Dashboard
+    status     : true, // check the login status upon init?
+    cookie     : true, // set sessions cookies to allow your server to access the session?
+    xfbml      : true  // parse XFBML tags on this page?
+  });
+
+  console.log('authd');
+
+  FB.login(function(response) {
+   if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log(response);
+       console.log('https://graph.facebook.com/' + response.username + '/picture?type=large')
+       console.log('Good to see you, ' + response.name + '.');
+     });
+   } else {
+     console.log('User cancelled login or did not fully authorize.');
+   }
+ });
+
+}
