4facb598caab78619e1150e23a3288601938345a
Otto Sipe <ottosipe@gmail.com>
2013-02-02 11:26:16 -0500

    working note sharing

--- public/js/main.js
+++ public/js/main.js
@@ -7,8 +7,8 @@ $(function(){
   console.log("Editing session", doc);
 
   if (doc == "" || doc == "#") window.location = "/"
-  
-  // *** Trains
+  var template = _.template($("#note-temp").html());
+  console.log($("#note-temp").html())
   var connection = sharejs.open(doc, 'json', function(error, doc) {
     if (error) {
       if (console) {
@@ -16,27 +16,56 @@ $(function(){
       }
       return;
     }
-    
+
     if (doc.created) {
-      doc.set(["Thomas the Tank Engine",
-        "Edward the Blue Engine",
-        "Henry the Green Engine",
-        "Gordon the Big Engine",
-        "James the Red Engine",
-        "Percy the Small Engine",
-        "The Fat Controller"
-      ]);
+      doc.set({"notes":[]});
+    } else {
+      var notes = doc.at('notes').get();
+      console.log(notes)
+      for(var i in notes) {
+        $("#notes").append(template(notes[i]))
+      }
     }
-    
-    var trains = doc.get();
-    $.each(trains, function(i, train) {
-      $('#trains').append($('<li>').text(trains[i])); //.attr('data-id', i)
+
+    var notes = doc.at('notes')
+    notes.on('insert', function (pos, item) {
+      $("#notes").append(template(item))
+      console.log(item);
     });
 
-    
+    $('#note').submit(function(e){
+      e.preventDefault();
+      var obj = {
+          text:$("#text").val()
+      }
+      notes.push(obj);
+      $("#notes").append(template(obj))
+      $("#text").val("")
+    });
+
+    // ???
+   /* notes.on('child op', function (path, op) {
+      var item_idx = path[0]
+      console.log("Item "+item_idx+" now reads "+notes.get()[item_idx])
+      if (op.si != undefined) {
+        // ...
+      } else if (op.sd != undefined) {
+        // ...
+      }
+    })*/
     
   });
 
+
+  // email
+  $('#addFriend').click(function() {
+    var email = prompt('Add your friend!');
+    $.post('/email', {email: email, name: 'your friend', doc: sessionHash}, function(data) {
+      console.log('added friend');
+    }); 
+  });
+
+  // status monitoring
   var status = document.getElementById('status');
   var register = function(state, klass, text) {
     connection.on(state, function() {
@@ -50,17 +79,4 @@ $(function(){
   register('disconnected', 'important', 'Offline');
   register('stopped', 'important', 'Error');
 
-
-
-  $('#addFriend').click(function() {
-    var email = prompt('Add your friend!');
-    $.post('/email', {email: email, name: 'your friend', doc: sessionHash}, function(data) {
-      console.log('added friend');
-    }); 
-  });
-
-
-  $("#syntaxSelect").change(function() {
-    console.log($(this).val())
-  });
 });
