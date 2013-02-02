348a8991c3382ce4b2a3e114a6f2fbf126bf1197
Matt Kneiser <mattman@umich.edu>
2013-02-02 06:14:46 -0500

    Sorry Otto, I'm switching off ShareJS

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,26 @@
 
 $(function(){
 
-  var doc = window.location.pathname;
-  doc = doc.replace(/\//g,"#");
+  var docName = window.location.pathname;
+  docName = docName.replace(/\//g,"#");
 
-  console.log("Editing session", doc);
+  console.log("Editing session", docName);
 
-  if (doc == "" || doc == "#") window.location = "/"
+  if (docName == "" || docName == "#"){window.location = "/";}
   
   // *** Trains
-  var connection = sharejs.open(doc, 'json', function(error, doc) {
+  /*var connection = sharejs.open(docName, 'json', function(error, doc) {
     if (error) {
       if (console) {
         console.error(error);
       }
       return;
     }
+
+    ///doc.attach_textarea(document.getElementById('input_box'));
     
     if (doc.created) {
-      doc.set(["Thomas the Tank Engine",
+      /*doc.set(["Thomas the Tank Engine",
         "Edward the Blue Engine",
         "Henry the Green Engine",
         "Gordon the Big Engine",
@@ -29,11 +31,56 @@ $(function(){
     }
     
     var trains = doc.get();
+    console.log(doc);
     $.each(trains, function(i, train) {
       $('#trains').append($('<li>').text(trains[i])); //.attr('data-id', i)
     });
 
-    
+    doc.on('change', function(c){
+      console.log('change');
+      console.log(c);
+    });
+
+    /*doc.on('insert', function(position, text) {
+      console.log('insert fired');
+    });
+    doc.on('remoteop', function(op) {
+      console.log('remoteop fired');
+    });
+
+    $('#input_box').focus(function(event){
+    console.log('event');
+     $('#input_box').keypress(function(k){
+      if(k.keyCode == 13){
+        //console.log('TEXT ENTERED');
+        //var delta_objects = [];
+        //var delta = {};
+        //delta.i = $('#input_box').val();
+        //delta.text = $('#input_box').val();
+        //delta.timestamp = new Date().getTime() / 1000; //UNIXTIME
+        //delta_objects.push(delta);
+        //doc.submitOp(delta);
+
+        //console.log(doc.length );
+        //console.log(doc);
+        console.log($('#input_box').val());
+        //console.log(doc.addAt(;1));
+        doc.addAt(doc.length, $('#input_box').val(), function(a, b){
+          console.log('cb');
+          console.log(a);
+          console.log(b);
+        });
+        /*doc.snapshot.push($('#input_box').val());
+        //console.log(doc.type);
+        console.log(doc.get());
+        //doc.submitOp({i:"Hi there!\n", p:0});
+
+        //document.getElementById('input_box').innerHTML = "";
+        //$('#input_box').html('');
+        //console.log($('#input_box'));
+      }
+    });
+  });
     
   });
 
@@ -62,5 +109,5 @@ $(function(){
 
   $("#syntaxSelect").change(function() {
     console.log($(this).val())
-  });
+  });*/
 });
--- server/app.js
+++ server/app.js
@@ -5,11 +5,12 @@ var express   = require('express')
   , router    = require('./router.js')
   , config    = require('./config.js')
   , http      = require('http')
-  , share 	  = require('share').server;
+  //, share 	  = require('share').server
+  , io        = require('socket.io');
 
 // setup here
 config(app);
-share.attach(app, {db: {type: 'none'}});
+//share.attach(app, {db: {type: 'none'}});
 
 // start the server
 var httpApp = http.createServer(app).listen(app.get('port'), function(){
@@ -17,8 +18,15 @@ var httpApp = http.createServer(app).listen(app.get('port'), function(){
 });
 
 // define API routes here
-app.get('/',            router.splash);
+app.get('/',          router.splash);
 app.get('/room/:id', 	router.room);
 
-
 app.post('/email',  router.email );
+
+var documents = [];
+
+var io = require('socket.io').listen(httpApp).set('log level', 1);
+
+io.sockets.on('connection', function(socket){
+  console.log('SOCKET CONNECTED'.green);
+});
