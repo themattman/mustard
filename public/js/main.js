15de246ce9d46ba6e5063ddb04d8bd75e4e84ac9
Matt Kneiser <mattman@umich.edu>
2013-02-03 00:50:59 -0500

    Client-side web scraping POSTer

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,13 @@ $(function(){
   var app = new MainView({model: new NoteList});
 
   $('.fb_pic').click(function(a){
-    var target = a.target.src.substr(a.target.src.indexOf('.com')+5);
-    var fb_username = target.substr(0, target.indexOf('/'));
-    console.log(fb_username);
-    window.location = "https://www.facebook.com/" + fb_username;
-  });
-
-  $.post('/scrape', {url: 'http://www.google.com'}, function(response){
-    console.log('response');
-    console.log(response);
+    $.post('/scrape', {url: 'http://www.cnn.com'}, function(response){
+      console.log('response');
+      console.log(response);
+      var img = document.createElement('img');
+      img.src = response[0].src;
+      document.getElementById('here').appendChild(img);
+    });
   });
 
 });
